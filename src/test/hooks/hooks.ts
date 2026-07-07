65225f2b84e428e9607fb81a2414c4b86c52e216
Shobana <shobanav112@gmail.com>
2026-07-07 13:12:32 +0530

    Added Hooks

--- src/test/hooks/hooks.ts
+++ src/test/hooks/hooks.ts
@@ -1,9 +1,40 @@
-import {Before,After,BeforeAll,AfterAll,Status} from "@cucumber/cucumber";
+import {Before,After,BeforeAll,AfterAll,Status} from '@cucumber/cucumber';
+import {Browser, chromium} from "@playwright/test";
+import { testWorld } from '../world/customworld';
+import {logger} from '../../utilities/logger';
 
-import { Browser, chromium } from "playwright";
 
-let browser: Browser;
 
-BeforeAll(async () => {
-    browser = await chromium.launch({ headless: false });
+
+let browser:Browser;
+BeforeAll(async()=>{
+    
+   browser = await chromium.launch({
+    headless: true
+  });
+    logger.info("Browser Launched");
+});
+Before(async function (this:testWorld,scenario) {
+    this.browser=browser;
+    this.context=await browser.newContext();
+    this.page=await this.context.newPage();
+    
+   
+});
+After(async function (this:testWorld,scenario) {
+    if(scenario.result?.status== Status.FAILED){
+        const path=`reports/screenshots/${scenario.pickle.name}${Date.now()}.png`;
+        await this.page.screenshot({path});
+       logger.error(`Scenario Failed: ${scenario.pickle.name}`);
+       logger.error(`Screenshot Saved:${path}`);
+}
+    else{
+       logger.info(`Scenario Passed:${scenario.pickle.name}`);
+    }
+     await this.page.close();
+    await this.context.close();
+})
+AfterAll(async () => {
+     logger.info("Browser Closed");
+    await browser.close();
 });
