82b517c921a6a40e97cf5425aae715c5470368e4
Shobana <shobanav112@gmail.com>
2026-07-07 12:43:57 +0530

    Added Logger

--- src/utilities/logger.ts
+++ src/utilities/logger.ts
@@ -1,1 +1,17 @@
-import loggers = require("winston");
+import winston from 'winston' ;
+export const logger = winston.createLogger({
+    level:'info',
+    format:winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.printf(({level,message,timestamp})=>{
+            return `[${timestamp}] ${level.toUpperCase()}-${message}` ;
+        })
+    ),
+    transports:[
+        new winston.transports.Console(),
+        new winston.transports.File({
+            filename:"reports/logs/test.log"
+        })
+    ]
+
+})
